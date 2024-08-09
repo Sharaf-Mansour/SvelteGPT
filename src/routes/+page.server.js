59bc523319ff1c60b6f15dcc561ce31ff3c5facb
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-08-09 16:04:16 +0300

    Ollama Svelte

---
+++ src/routes/+page.server.js
@@ -0,0 +1,21 @@
+import ollama from 'ollama';
+import { toolsString, executeFunction } from '$lib';
+
+const systemPrompt = `You are a helpful assistant that takes a question and finds the most appropriate tool or tools to execute, along with the parameters required to run the tool. Respond as JSON using the following schema: {"functionName": "function name", "parameters": [{"parameterName": "name of parameter", "parameterValue": "value of parameter"}]}. The tools are: ${toolsString}`;
+
+export const actions = {
+	prompt: async ({ cookies, request }) => {
+		const { model, prompt } = Object.fromEntries(await request.formData());
+		const response = await ollama.generate({
+			model: model,
+			system: systemPrompt,
+			prompt: prompt,
+			stream: false,
+			format: 'json'
+		});
+		const responseObject = JSON.parse(response.response.trim());
+		await executeFunction(responseObject.functionName, responseObject.parameters);
+		const jsonStr = JSON.stringify(responseObject, null, 2);
+		return { response: jsonStr };
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { enhance } from '$app/forms';
+	import Highlight, { LineNumbers } from 'svelte-highlight';
+	import json from 'svelte-highlight/languages/json';
+	import github from 'svelte-highlight/styles/github';
+	const models = [
+		{
+			name: 'codegeex4'
+		},
+		{
+			name: 'deepseek-coder-v2'
+		},
+		{
+			name: 'codestral'
+		},
+		{
+			name: 'llama3'
+		}
+	];
+	let { form } = $props();
+</script>
+
+<svelte:head>
+	{@html github}
+</svelte:head>
+<h1>SvelteGPT</h1>
+
+<form action="?/prompt" method="post" use:enhance>
+	<select name="model">
+		{#each models as model (model.name)}
+			<option value={model.name}>{model.name}</option>
+		{/each}
+	</select>
+	<br />
+	<textarea name="prompt" placeholder="Prompt"> </textarea>
+	<button type="submit">Submit</button>
+</form>
+
+{#if form}
+	<h2>Response</h2>
+	<Highlight language={json} code={form.response} let:highlighted>
+		<LineNumbers {highlighted} />
+	</Highlight>
+{/if}
